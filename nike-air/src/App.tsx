281c3e762be0a9cf7ebb68a7a8f8599df26ab85c
Ana Queiroz <acm.queiroz@outlook.com>
2025-09-25 11:27:32 -0300

    Initial commit

---
+++ nike-air/src/App.tsx
@@ -0,0 +1,20 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+           <img src="src/assets/logo (1).png"></img>
+       
+      <h1>Nike Air</h1>
+      <h2> R$ 799,90</h2>
+
+      <p> Uma passada e</p>
+     
+    </>
+  )
+}
+
+export default App
